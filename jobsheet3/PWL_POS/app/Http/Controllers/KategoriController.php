7ef25818695ac22552387b10b6e7d5e56aedbffc
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2026-04-19 15:36:44 +0700

    praktikum 5 query : update kategori 1 baris.

--- jobsheet3/PWL_POS/app/Http/Controllers/KategoriController.php
+++ jobsheet3/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -9,14 +9,11 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        /* data yang akan ditambahkan ke tabel m_kategori */
-        $data = [
-            'kategori_kode' => 'SNK',
-            'kategori_nama' => 'Snack/Makanan Ringan',
-            'created_at' => now()
-        ];
-        
-        DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil';
+        // $data = ['kategori_kode' => 'SNK', 'kategori_nama' => 'Snack/Makanan Ringan', 'created_at' => now()];
+        // DB::table('m_kategori')->insert($data);
+
+        // Update data kategori
+        $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris.';
     }
 }
