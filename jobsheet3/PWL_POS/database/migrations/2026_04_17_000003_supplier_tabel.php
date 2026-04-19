e95da7d7c53c659f46411245f3d3a61ffcf0a68d
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2026-04-19 14:01:17 +0700

    db edit

--- jobsheet3/PWL_POS/database/migrations/2026_04_17_000003_supplier_tabel.php
+++ jobsheet3/PWL_POS/database/migrations/2026_04_17_000003_supplier_tabel.php
@@ -9,10 +9,13 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+   public function up(): void
     {
         Schema::create('m_supplier', function (Blueprint $table) {
-            $table->id();
+            $table->id('supplier_id'); // <-- Pastikan ini benar
+            $table->string('supplier_kode', 10)->unique();
+            $table->string('supplier_nama', 100);
+            $table->string('supplier_alamat', 255);
             $table->timestamps();
         });
     }
