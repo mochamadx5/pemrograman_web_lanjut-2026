2f8907c9f4e9c68a5cf3c4ba4e74385468e09e79
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2026-04-19 16:24:40 +0700

    praktikum 2_Retrieving Single Models

--- PWL_POS/Jobsheet_3/app/Http/Controllers/UserController.php
+++ PWL_POS/Jobsheet_3/app/Http/Controllers/UserController.php
@@ -8,19 +8,9 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-   public function index()
+    public function index()
     {
-        // Matikan kode ini agar tidak terus-terusan menambah data yang sama
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_dua',
-        //     'nama' => 'Manager 2',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::create($data);
-
-        // Coba ambil semua data user
-        $user = UserModel::all();
+        $user = UserModel::find(1); 
         return view('user', ['data' => $user]);
     }
 }
---
+++ PWL_POS/Jobsheet_4/readme.md
@@ -0,0 +1,15 @@
+# Laporan Praktikum - Jobsheet 4: MODEL dan ELOQUENT ORM
+
+---
+
+## Identitas Mahasiswa
+* **Nama:** Mochamad Reza Firdaus
+* **NIM:** 244107020104
+* **Kelas:** TI-2F
+* **Project:** PWL_POS
+
+---
+
+## Praktikum 1 - $fillable:
+**Menampilkan Data**
+![View](img/1.png)
