8b17fa9c96d9ad2ebf40eaa452863f1fdf8dd31c
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2026-06-01 14:59:55 +0700

    jobsheet 15 : update database

--- jobsheet5_filament/database/migrations/2026_05_25_042237_create_posts_table.php
+++ jobsheet5_filament/database/migrations/2026_05_25_042237_create_posts_table.php
@@ -11,29 +11,43 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // 1. Tabel Posts (Kolom JSON tags sudah dihapus)
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('slug');
-    
-            // Baris ini membuat foreign key yang terhubung ke tabel categories
             $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete(); 
-    
             $table->string('color')->nullable();
             $table->string('image')->nullable();
             $table->text('body')->nullable();
-            $table->json('tags')->nullable();
             $table->boolean('published')->default(false);
             $table->date('published_at')->nullable();
             $table->timestamps();
         });
-    } // <--- INI YANG KURANG TADI
+
+        // 2. Tabel Tags baru
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        // 3. Pivot Table (Tabel Penghubung post_tag)
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
+            $table->primary(['post_id', 'tag_id']);
+        });
+    } 
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
+        // Hapus dari anak ke induk (urutan terbalik)
+        Schema::dropIfExists('post_tag');
+        Schema::dropIfExists('tags');
         Schema::dropIfExists('posts');
     }
 };
